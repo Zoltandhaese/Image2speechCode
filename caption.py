5027a5e9ff6d067ee74ff635b425580f134b7403
zoltandhaese <zdhaese@gmail.com>
2020-03-11 21:27:05 +0100

    Eval.py is fully working + first phase of implementation of caption

--- caption.py
+++ caption.py
@@ -9,11 +9,19 @@ import skimage.transform
 import argparse
 from scipy.misc import imread, imresize
 from PIL import Image
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+from datasets import *
+from utils import *
+from tqdm import tqdm
+import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=3):
+def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=1):
     """
     Reads an image and captions it with beam search.
 
@@ -29,18 +37,19 @@ def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=
     vocab_size = len(word_map)
 
     # Read image and process
-    img = imread(image_path)
-    if len(img.shape) == 2:
-        img = img[:, :, np.newaxis]
-        img = np.concatenate([img, img, img], axis=2)
-    img = imresize(img, (256, 256))
-    img = img.transpose(2, 0, 1)
-    img = img / 255.
-    img = torch.FloatTensor(img).to(device)
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-    transform = transforms.Compose([normalize])
-    image = transform(img)  # (3, 256, 256)
+    
+    #img = imread(image_path)
+    #if len(img.shape) == 2:
+     #   img = img[:, :, np.newaxis]
+      #  img = np.concatenate([img, img, img], axis=2)
+    #mg = imresize(img, (256, 256))
+    #img = img.transpose(2, 0, 1)
+    #img = img / 255.
+    #img = torch.FloatTensor(img).to(device)
+    #normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     #std=[0.229, 0.224, 0.225])
+    #transform = transforms.Compose([normalize])
+    image = image_path  # (3, 256, 256)
 
     # Encode
     image = image.unsqueeze(0)  # (1, 3, 256, 256)
@@ -146,6 +155,136 @@ def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=
 
     return seq, alphas
 
+def caption_old_fashion(encoder, decoder, image, word_map, beam_size=1):
+
+    k = beam_size
+
+    # Move to GPU device, if available
+    image = image.to(device)  # (1, 3, 256, 256)
+
+    # Encode
+    """
+    encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
+    enc_image_size = encoder_out.size(1)
+    encoder_dim = encoder_out.size(3)
+
+    # Flatten encoding
+    encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
+    num_pixels = encoder_out.size(1)
+
+    # We'll treat the problem as having a batch size of k
+    encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
+    """
+    encoder_out = encoder(image)
+    image = image.unsqueeze(0)  # (1, 3, 256, 256)
+    encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
+    enc_image_size = encoder_out.size(1)
+    encoder_dim = encoder_out.size(3)
+    # Tensor to store top k previous words at each step; now they're just <start>
+    k_prev_words = torch.LongTensor([[word_map['<start>']]] * k).to(device)  # (k, 1)
+
+    # Tensor to store top k sequences; now they're just <start>
+    seqs = k_prev_words  # (k, 1)
+
+    # Tensor to store top k sequences' scores; now they're just 0
+    top_k_scores = torch.zeros(k, 1).to(device)  # (k, 1)
+    seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(device)
+    # Lists to store completed sequences and scores
+    complete_seqs = list()
+    complete_seqs_scores = list()
+
+    # Start decoding
+    step = 1
+    h, c = decoder.init_hidden_state(encoder_out)
+
+    # s is a number less than or equal to k, because sequences are removed from this process once they hit <end>
+    while True:
+
+        embeddings = decoder.embedding(k_prev_words).squeeze(1)  # (s, embed_dim)
+
+        awe, alpha = decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
+        
+        alpha = alpha.view(-1, enc_image_size, enc_image_size)
+
+        gate = decoder.sigmoid(decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
+        awe = gate * awe
+
+        h, c = decoder.decode_step(torch.cat([embeddings, awe], dim=1), (h, c))  # (s, decoder_dim)
+
+        scores = decoder.fc(h)  # (s, vocab_size)
+        scores = F.log_softmax(scores, dim=1)
+
+        # Add
+        scores = top_k_scores.expand_as(scores) + scores  # (s, vocab_size)
+
+        # For the first step, all k points will have the same scores (since same k previous words, h, c)
+        if step == 1:
+            top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (s)
+        else:
+            # Unroll and find top scores, and their unrolled indices
+            top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)  # (s)
+
+        # Convert unrolled indices to actual indices of scores
+        prev_word_inds = top_k_words / vocab_size  # (s)
+        next_word_inds = top_k_words % vocab_size  # (s)
+
+        # Add new words to sequences
+        seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
+        seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],
+                                dim=1)
+                                
+        # Which sequences are incomplete (didn't reach <end>)?
+        incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
+                            next_word != word_map['<end>']]
+        complete_inds = list(set(range(len(next_word_inds))) - set(incomplete_inds))
+
+        # Set aside complete sequences
+        if len(complete_inds) > 0:
+            complete_seqs.extend(seqs[complete_inds].tolist())
+            complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
+            complete_seqs_scores.extend(top_k_scores[complete_inds])
+        k -= len(complete_inds)  # reduce beam length accordingly
+
+        # Proceed with incomplete sequences
+        if k == 0:
+            break
+        seqs = seqs[incomplete_inds]
+        seqs_alpha = seqs_alpha[incomplete_inds]
+        h = h[prev_word_inds[incomplete_inds]]
+        c = c[prev_word_inds[incomplete_inds]]
+        encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
+        top_k_scores = top_k_scores[incomplete_inds].unsqueeze(1)
+        k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
+
+        # Break if things have been going on too long
+        if step > 50:
+            break
+        step += 1
+
+    if len(complete_seqs_scores)>0:
+        i = complete_seqs_scores.index(max(complete_seqs_scores))
+        
+        seq = complete_seqs[i] # zal onze referentie worden.
+        alphas = complete_seqs_alpha[i]
+    else :
+        seq=[]
+
+    # References
+    """
+    img_caps = allcaps[0].tolist()
+    img_captions = list(
+        map(lambda c: [w for w in c if w not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}],
+            img_caps))  # remove <start> and pads
+    references.append(img_captions)       """
+    # Hypotheses
+    hypotheses = list()
+    phone_cap = [tophone[str(seq[i])] for i in range(1,len(seq)-1)]    #de bedoeling is dus om voor de lijst caps, dit is een lijst van nummers om te zetten naar phonemes
+    new_phone_cap = [tophone[str(seq[i])] + ' ' for i in range(1,len(seq)-1)] 
+    hypotheses.append(phone_cap)
+    
+    return hypotheses, alphas
+
+
 
 def visualize_att(image_path, seq, alphas, rev_word_map, smooth=True):
     """
@@ -187,17 +326,26 @@ def visualize_att(image_path, seq, alphas, rev_word_map, smooth=True):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Show, Attend, and Tell - Tutorial - Generate Caption')
-
-    parser.add_argument('--img', '-i', help='path to image')
-    parser.add_argument('--model', '-m', help='path to model')
-    parser.add_argument('--word_map', '-wm', help='path to word map JSON')
-    parser.add_argument('--beam_size', '-b', default=5, type=int, help='beam size for beam search')
-    parser.add_argument('--dont_smooth', dest='smooth', action='store_false', help='do not smooth alpha overlay')
-
+    #parser.add_argument('--img', '-i', help='path to image')
+    #parser.add_argument('--model', '-checkpoint_0.pth.tar', help='path to model')
+    #parser.add_argument('--word_map', '-wm', help='path to word map JSON')
+    #parser.add_argument('--beam_size', '-b', default=1, type=int, help='beam size for beam search')
+    #parser.add_argument('--dont_smooth', dest='smooth', action='store_false', help='do not smooth alpha overlay')
+    data_path = 'dataset'  # folder with data files saved by create_input_files.py
+    checkpoint = 'checkpoint_0.pth.tar'  # model checkpoint
+    word_map_file =  data_path + '/' + 'caps_dic.pickle'
     args = parser.parse_args()
+    with open("New_model/imagenames.txt") as f:
+        names = f.readlines()
+    i = random.randint(0,4000)
+    imgname = names[i][:-1] #okay dus het zijn altijd herhalingen van 5 ook dus de names!
+    path_to_Image = "New_model/Flicker8k_Dataset/" + imgname +".jpg"
+    beam_size= 1
+    batch_size = 1
+    workers = 0
 
     # Load model
-    checkpoint = torch.load(args.model)
+    checkpoint = torch.load(checkpoint)
     decoder = checkpoint['decoder']
     decoder = decoder.to(device)
     decoder.eval()
@@ -206,13 +354,21 @@ if __name__ == '__main__':
     encoder.eval()
 
     # Load word map (word2ix)
-    with open(args.word_map, 'r') as j:
-        word_map = json.load(j)
+    with open(word_map_file, 'rb') as j:
+        word_map = pickle.load(j)
     rev_word_map = {v: k for k, v in word_map.items()}  # ix2word
 
+    loader = torch.utils.data.DataLoader(
+        CaptionDataset(data_path,split='VAL'),
+        batch_size=batch_size, shuffle=False, num_workers=workers,collate_fn=pad_collate_train, pin_memory=True)
+    Images=[]
+    for i, (image, caps, caplens) in enumerate(
+            tqdm(loader, desc="EVALUATING AT BEAM SIZE " + str(beam_size))):
+        Images.append(image)
+    Image1=Images[i]
     # Encode, decode with attention and beam search
-    seq, alphas = caption_image_beam_search(encoder, decoder, args.img, word_map, args.beam_size)
+    seq, alphas = caption_old_fashion(encoder, decoder, Image1, word_map, beam_size)
     alphas = torch.FloatTensor(alphas)
 
     # Visualize caption and attention of best sequence
-    visualize_att(args.img, seq, alphas, rev_word_map, args.smooth)
+    visualize_att(path_to_Image, seq, alphas, rev_word_map, args.smooth)
--- eval.py
+++ eval.py
@@ -14,12 +14,12 @@ workers = 0
 
 data_path = 'dataset'  # folder with data files saved by create_input_files.py
 # data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
-checkpoint = 'checkpoint_0.pth.tar'  # model checkpoint
+checkpoint = 'checkpoint_6.pth.tar'  # model checkpoint
 word_map_file =  data_path + '/' + 'caps_dic.pickle' # word map, dus word => index, ensure it's the same the data was encoded with and the model was trained with
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
-tophones_path = data_path + '/' + 'tophone_dic.pickle'
+tophones_path = data_path + '/' + 'tophone_dic.pickle' 
 with open(tophones_path,'rb') as f:
     tophone = pickle.load(f)# to phone: getal=> naar een phoneem
 
@@ -38,13 +38,6 @@ with open(word_map_file, 'rb') as f:
 rev_word_map = {v: k for k, v in word_map.items()}
 vocab_size = len(word_map)
 
-
-
-# Normalization transform
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-
-
 def evaluate(beam_size):
     """
     Evaluation
@@ -52,20 +45,10 @@ def evaluate(beam_size):
     :param beam_size: beam size at which to generate captions for evaluation
     :return: BLEU-4 score
     """
-    # DataLoader
-    # loader = torch.utils.data.DataLoader(
-    #     CaptionDataset(data_folder, data_name, 'TEST', transform=transforms.Compose([normalize])),
-    #     batch_size=1, shuffle=True, num_workers=1, pin_memory=True)
-
     loader = torch.utils.data.DataLoader(
         CaptionDataset(data_path,split='VAL'),
         batch_size=batch_size, shuffle=False, num_workers=workers,collate_fn=pad_collate_train, pin_memory=True)
-    # TODO: Batched Beam Search
-    # Therefore, do not use a batch_size greater than 1 - IMPORTANT!
 
-    # Lists to store references (true captions), and hypothesis (prediction) for each image
-    # If for n images, we have n hypotheses, and references a, b, c... for each image, we need -
-    # references = [[ref1a, ref1b, ref1c], [ref2a, ref2b], ...], hypotheses = [hyp1, hyp2, ...]
     references = list()
     hypotheses = list()
 
@@ -91,7 +74,10 @@ def evaluate(beam_size):
         # We'll treat the problem as having a batch size of k
         encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
         """
-        encoder_out = encoder(image)
+        encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
+        #enc_image_size = encoder_out.size(1)
+       
+    
         # Tensor to store top k previous words at each step; now they're just <start>
         k_prev_words = torch.LongTensor([[word_map['<start>']]] * k).to(device)  # (k, 1)
 
@@ -100,6 +86,9 @@ def evaluate(beam_size):
 
         # Tensor to store top k sequences' scores; now they're just 0
         top_k_scores = torch.zeros(k, 1).to(device)  # (k, 1)
+
+        # Tensor to store top k sequences' alphas; now they're just 1s
+        #seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(device) 
 
         # Lists to store completed sequences and scores
         complete_seqs = list()
@@ -116,6 +105,8 @@ def evaluate(beam_size):
 
             awe, _ = decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
 
+           # alpha = alpha.view(-1, enc_image_size, enc_image_size) 
+
             gate = decoder.sigmoid(decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
             awe = gate * awe
 
@@ -124,12 +115,13 @@ def evaluate(beam_size):
             scores = decoder.fc(h)  # (s, vocab_size)
             scores = F.log_softmax(scores, dim=1)
 
-            # Add
+            # At
             scores = top_k_scores.expand_as(scores) + scores  # (s, vocab_size)
 
             # For the first step, all k points will have the same scores (since same k previous words, h, c)
             if step == 1:
                 top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (s)
+               # print ("stap 1")
             else:
                 # Unroll and find top scores, and their unrolled indices
                 top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)  # (s)
@@ -137,10 +129,12 @@ def evaluate(beam_size):
             # Convert unrolled indices to actual indices of scores
             prev_word_inds = top_k_words / vocab_size  # (s)
             next_word_inds = top_k_words % vocab_size  # (s)
-
+            if step > 100:
+                next_word_inds=torch.tensor([word_map['<end>']]).to(device)
+            
             # Add new words to sequences
             seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
-
+            #seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],dim=1)  # (s, step+1, enc_image_size, enc_image_size)
             # Which sequences are incomplete (didn't reach <end>)?
             incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
                                next_word != word_map['<end>']]
@@ -149,6 +143,7 @@ def evaluate(beam_size):
             # Set aside complete sequences
             if len(complete_inds) > 0:
                 complete_seqs.extend(seqs[complete_inds].tolist())
+               # complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
                 complete_seqs_scores.extend(top_k_scores[complete_inds])
             k -= len(complete_inds)  # reduce beam length accordingly
 
@@ -156,6 +151,7 @@ def evaluate(beam_size):
             if k == 0:
                 break
             seqs = seqs[incomplete_inds]
+            #seqs_alpha = seqs_alpha[incomplete_inds]
             h = h[prev_word_inds[incomplete_inds]]
             c = c[prev_word_inds[incomplete_inds]]
             encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
@@ -163,15 +159,16 @@ def evaluate(beam_size):
             k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
 
             # Break if things have been going on too long
-            if step > 50:
+            if step > 100:
                 break
             step += 1
 
-        if len(complete_seqs_scores)>0:
-            i = complete_seqs_scores.index(max(complete_seqs_scores))
-            seq = complete_seqs[i] # zal onze referentie worden.
-        else :
-            seq=[]
+        #if len(complete_seqs_scores)>0:
+        i = complete_seqs_scores.index(max(complete_seqs_scores))
+        seq = complete_seqs[i] # zal onze referentie worden.
+            #alphas = complete_seqs_alpha[i]
+        #else :
+         #   seq=[]
 
         # References
         """
@@ -181,11 +178,11 @@ def evaluate(beam_size):
                 img_caps))  # remove <start> and pads
         references.append(img_captions)       """
         # Hypotheses
-        phone_cap = [tophone[str(seq[i])] for i in range(len(seq))]    #de bedoeling is dus om voor de lijst caps, dit is een lijst van nummers om te zetten naar phonemes
-        new_phone_cap = [tophone[str(seq[i])] + ' ' for i in range(len(seq))] 
+        phone_cap = [tophone[str(seq[i])] for i in range(1,len(seq)-1)]    #de bedoeling is dus om voor de lijst caps, dit is een lijst van nummers om te zetten naar phonemes
+        new_phone_cap = [tophone[str(seq[i])] + ' ' for i in range(1,len(seq)-1)] 
         hypotheses.append(phone_cap)
 
-        with open("test_0_e.txt", "a") as f:
+        with open("test_e7_l100.txt", "a") as f:
             if (len(new_phone_cap) > 0):
                 for i in range(len(new_phone_cap)-1):
                     f.write(new_phone_cap[i])
@@ -194,6 +191,7 @@ def evaluate(beam_size):
                 f.write("no results :( " + "\n")  
 
         #hypotheses.append([w for w in seq if w not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}])
+        
         num = caps[0]
         phone_cap = [tophone[str(int(num[i].tolist()[0]))] for i in range(len(num))]    #de bedoeling is dus om voor de lijst caps, dit is een lijst van nummers om te zetten naar phonemes
         new_phone_cap = [tophone[str(int(num[i].tolist()[0]))] + ' ' for i in range(len(num))]  #[:length] 
